ff4296ab5e673c1b26636b62a35e5a1da4bc8741
pengbo.yang <obpt@sina.com>
2020-11-04 08:44:12 +0800

    add unit test

---
+++ test/YS.AppVersion.Impl.Assembly.UnitTest/AssemblyAppVersionServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YS.AppVersion.Impl.Assembly.UnitTest
+{
+    [TestClass]
+    public class AssemblyAppVersionServiceTest
+    {
+        [TestMethod]
+        public async Task ShouldGetAssemblyVersion()
+        {
+            var service = new AssemblyAppVersionService();
+            var version = await service.GetVersion();
+            Assert.IsNotNull(version);
+            Assert.IsNotNull(version.AssemblyVersion);
+            Assert.IsNotNull(version.AssemblyFileVersion);
+            Assert.IsNotNull(version.AssemblyInformationalVersion);
+        }
+    }
+}
